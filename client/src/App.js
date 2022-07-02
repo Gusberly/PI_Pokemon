74f981d1219c643b0b754861d705b3f8d238152e
Gusberly Morillo <Gusberlymorillo@gmail.com>
2022-07-01 22:16:57 -0300

    terminado

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from "./Components/LandingPage/Landing";
+import Home from "./Components/Home/Home";
+import { PokemonCreado } from "./Components/Create/Create";
+import { PokeDetalles } from "./Components/DetallesPoke/Detalles";
+import { Error404 } from "./Components/error404/error404";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/home" component={Home} />
+          <Route path="/post" component={PokemonCreado} />
+          <Route path="/pokemon/:id" component={PokeDetalles} />
+          <Route path="*" component={Error404} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
